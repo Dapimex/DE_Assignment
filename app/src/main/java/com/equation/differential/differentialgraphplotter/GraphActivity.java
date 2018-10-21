6965d383c99dd303be175d609df408bf11f00844
Dapimex <d.pimenov@innopolis.ru>
2018-10-21 13:12:58 +0300

    Graph of local errors added

--- app/src/main/java/com/equation/differential/differentialgraphplotter/GraphActivity.java
+++ app/src/main/java/com/equation/differential/differentialgraphplotter/GraphActivity.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 public class GraphActivity extends AppCompatActivity {
 
     GraphView plotter;
+    GraphView error;
 
     ArrayList<DataPoint> dataPoints;
 
@@ -23,6 +24,10 @@ public class GraphActivity extends AppCompatActivity {
     LineGraphSeries<DataPoint> impEuler;
 
     LineGraphSeries<DataPoint> runge;
+
+    LineGraphSeries<DataPoint> errorEuler;
+    LineGraphSeries<DataPoint> errorImpEuler;
+    LineGraphSeries<DataPoint> errorRunge;
 
     private double step;
     private double x0;
@@ -51,10 +56,20 @@ public class GraphActivity extends AppCompatActivity {
         DataPoint[] dp = euler(x0, y0, x_fin, step);
         DataPoint[] dp2 = impEuler(x0, y0, x_fin, step);
         DataPoint[] dp3 = runge(x0, y0, x_fin, step);
+        DataPoint[] exactEuler = exactGraph(dp);
+        DataPoint[] exactImpEuler = exactGraph(dp2);
+        DataPoint[] exactRunge = exactGraph(dp3);
+        DataPoint[] er = error(dp, exactEuler);
+        DataPoint[] er2 = error(dp2, exactImpEuler);
+        DataPoint[] er3 = error(dp3, exactRunge);
 
         euler = new LineGraphSeries<>(dp);
         impEuler = new LineGraphSeries<>(dp2);
         runge = new LineGraphSeries<>(dp3);
+
+        errorEuler = new LineGraphSeries<>(er);
+        errorImpEuler = new LineGraphSeries<>(er2);
+        errorRunge = new LineGraphSeries<>(er3);
 
         euler.setColor(Color.RED);
         impEuler.setColor(Color.GREEN);
@@ -63,7 +78,16 @@ public class GraphActivity extends AppCompatActivity {
         impEuler.setTitle("Improved Euler's Method");
         runge.setTitle("Runge-Kutta's Method");
 
+        errorEuler.setColor(Color.RED);
+        errorImpEuler.setColor(Color.GREEN);
+        errorRunge.setColor(Color.BLUE);
+        errorEuler.setTitle("Euler's Error");
+        errorImpEuler.setTitle("Improved Euler's Error");
+        errorRunge.setTitle("Runge-Kutta's Error");
+
         plotter = findViewById(R.id.plotter);
+        plotter.setTitle(getResources().getString(R.string.plot_name));
+
         plotter.addSeries(euler);
         plotter.addSeries(impEuler);
         plotter.addSeries(runge);
@@ -76,12 +100,33 @@ public class GraphActivity extends AppCompatActivity {
 
         plotter.getLegendRenderer().setVisible(true);
         plotter.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+
+        error = findViewById(R.id.error);
+        error.setTitle(getResources().getString(R.string.error_name));
+
+        error.addSeries(errorEuler);
+        error.addSeries(errorImpEuler);
+        error.addSeries(errorRunge);
+
+        error.getViewport().setScalable(true);
+        error.getViewport().setScalableY(true);
+        error.getViewport().setScrollable(true);
+        error.getViewport().setScrollableY(true);
+
+
+        error.getLegendRenderer().setVisible(true);
+        error.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
     }
 
+    private double ivp(double x, double y) {
+        return Math.pow(y, 2)/Math.pow(x, 2) - 2*Math.log(x);
+    }
     private double funct(double x, double y) {
         return y/x + x/y;
     }
-
+    private double exact_solution(double x,  double c) {
+        return x*Math.sqrt(c + Math.log(x));
+    }
     private DataPoint[] euler(Double x0, Double y0, double x_fin, double step) {
         dataPoints.clear();
         while (x0 <= x_fin) {
@@ -128,5 +173,27 @@ public class GraphActivity extends AppCompatActivity {
 
         return res;
     }
-
+    private DataPoint[] exactGraph(DataPoint[] method) {
+        DataPoint[] res = new DataPoint[method.length];
+        double c = ivp(method[0].getX(), method[0].getY());
+        for (int i = 0; i < method.length; i++) {
+            res[i] = new DataPoint(method[i].getX(), exact_solution(method[i].getX(), c));
+        }
+        return res;
+    }
+    private DataPoint[] error(DataPoint[] graph, DataPoint[] exact) {
+        if (!(graph.length == exact.length)) {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.not_match_length), Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        DataPoint[] res = new DataPoint[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[i].getX() != exact[i].getX()) {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.not_match_x), Toast.LENGTH_SHORT).show();
+                return null;
+            }
+            res[i] = new DataPoint(graph[i].getX(), Math.abs(graph[i].getY() - exact[i].getY()));
+        }
+        return res;
+    }
 }
